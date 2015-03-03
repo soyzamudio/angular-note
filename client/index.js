566b8a17ed81e51ee24aa5f389d94d87bc87487d
Jose Zamudio <zamudio@outlook.com>
2015-03-03 14:10:27 -0800

    Create angular routes

---
+++ client/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('angular-notes', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('home', { url: '/', templateUrl: '/views/general/home.html' })
+      .state('faq', { url: '/faq', templateUrl: '/views/general/faq.html' })
+      .state('contact', { url: '/contact', templateUrl: '/views/general/contact.html' })
+      .state('register', { url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl' })
+
+    if(window.history && window.history.pushState) {
+      $locationProvider.html5Mode({
+             enabled: true,
+             requireBase: false
+      });
+    }
+  }])
---
+++ client/models/user.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('angular-notes')
+  .factory('User', ['$http', function($http) {
+
+    function register(user) {
+      return $http.post('/register', user);
+    }
+
+    return {register:register};
+
+  }]);
